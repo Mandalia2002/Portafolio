48bbf66e57ded306fb5cf05c120e9c3027f3590b
Mandalia2002 <sofiapg.2002@hotmail.com>
2025-11-12 16:29:43 -0800

    Feature(app): Menu iniciated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Presentation } from './presentation/presentation';
+import { Resume } from './resume/resume';
+import { Portafolio } from './portafolio/portafolio';
+import { Comissions } from './comissions/comissions';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Presentation},
+    {path: 'resume', component: Resume},
+    {path: 'portafolio', component: Portafolio},
+    {path: 'comissions', component: Comissions},
+    {path: 'contact', component: Contact}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Portafolio');
+  protected readonly title = signal('Mandalia Garciel');
 }
