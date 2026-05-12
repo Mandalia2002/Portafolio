3ec338bcc74cef331553a9bf33b8932e9735be63
Mandalia2002 <sofiapg.2002@hotmail.com>
2026-05-12 08:41:02 -0700

    Feature(skills): Skills Page Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Resume } from './resume/resume';
 import { Portafolio } from './portafolio/portafolio';
 import { Comissions } from './comissions/comissions';
 import { Contact } from './contact/contact';
+import { Skills } from './skills/skills';
 
 export const routes: Routes = [
     {path: '', component: Presentation},
     {path: 'resume', component: Resume},
+    {path: 'skills', component: Skills},
     {path: 'portafolio', component: Portafolio},
     {path: 'comissions', component: Comissions},
     {path: 'contact', component: Contact}
