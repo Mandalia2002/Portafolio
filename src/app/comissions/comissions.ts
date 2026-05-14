68c7e9cd0a03d0b3af21574c6d5d278a62b33e8d
Mandalia2002 <sofiapg.2002@hotmail.com>
2026-05-14 13:54:28 -0700

    Feature(comissions): Comission Calculator <3

--- src/app/comissions/comissions.ts
+++ src/app/comissions/comissions.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-comissions',
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './comissions.scss',
 })
 export class Comissions {
+  items = signal<{ name: string, price: number }[]>([]);
 
+  prices: Record<string, number> = {
+    'Color': 20.00,
+    'B&W': 10.00,
+    'Bust': 15.00,
+    'Full Body': 35.00,
+    'Extra Character': 15.00,
+    'Background': 25.00
+  };
+
+  total = computed(() => {
+    return this.items().reduce((acc, item) => acc + item.price, 0);
+  });
+
+  add(itemName: string) {
+    const price = this.prices[itemName] || 0;
+    this.items.update(prev => [...prev, { name: itemName, price: price }]);
+  }
+
+  remove(index: number) {
+    this.items.update(prev => prev.filter((_, i) => i !== index));
+  }
+
+  clear() {
+    this.items.set([]);
+  }
 }
